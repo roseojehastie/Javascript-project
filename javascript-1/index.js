1224fcd7d698f542e90adebcd79793ae9de810c2
Rose Hastie <roseojehastie@gmail.com>
2021-07-03 23:24:05 +0100

    javascript-1-operators

---
+++ javascript-1/index.js
@@ -0,0 +1,36 @@
+let name = 'rose';
+console.log(name);
+
+let favouriteNumber = 42;
+console.log(favouriteNumber)
+
+let names = "peter";
+console.log(names);
+
+var car = "lamborgini";
+console.log(car);
+
+const nan = ("Oje's");
+console.log(nan);
+
+//Basic aritemetic in javascript with built in operators  //
+console.log(5+6);
+console.log(10 / 5); 
+console.log(5 % 2); 
+console.log(8 ** 2);
+
+//  COComparison opeerator to COMPARE values. === checks for strict equality//
+console.log('cat' === 'cat')
+console.log('cat' === 'dog')
+console.log('dog' === 'dog')
+
+//putting a BANG, or EXCLAMATION MARK, IN FRONT OF A DOUBLE EQUALS TESTS FOR STRICT INEAQUALITY//
+console.log('cat' !== 'dog')
+console.log('cat' !== 'cat')
+
+//test whether a vlaue is greater than, greater than or equal   //
+console.log(5 < 10)
+console.log(5 <= 5)
+console.log(3 >= 2)
+console.log(2 > 2)
+console.log(3 > 2)
