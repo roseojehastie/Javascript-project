c88311651fae1c04d1ce1d314e1657b28681684b
Rose Hastie <roseojehastie@gmail.com>
2021-07-22 10:35:58 +0100

    reassigning variable

--- js-freeCodeCamp/index.js
+++ js-freeCodeCamp/index.js
@@ -17,12 +17,40 @@
 // let count = firstBatch + secondBatch
 // console.log(count)
 
-//1. Create two variables, myAge and humanDogRatio
-//2. Multiply the two together and store the result in myDogAge
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
 // 3. Log my DogAge to the console
 
-let myAge = 35
-let humanDogRatio = 7
+// let myAge = 35
+// let humanDogRatio = 7
  
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+//reassigment of OR CHANGING variable 
+// let count = 5
+// // count = 3
+// console.log(count)
+
+//INCREMENT VARIABLE BY REASSIGNING  
+let count = 5
+// COUNT + 1
+ count =  count + 33
+ count =  count - 33 
+console.log(count)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 100
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 25
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 70
+
+console.log(bonusPoints)
+
+
+
