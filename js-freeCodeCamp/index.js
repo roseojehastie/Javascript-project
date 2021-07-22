13ca4e3dfb40eb42e622fc5cff9e37bcdcbca574
Rose Hastie <roseojehastie@gmail.com>
2021-07-22 10:10:01 +0100

    add assigning values to variable

--- js-freeCodeCamp/index.js
+++ js-freeCodeCamp/index.js
@@ -1,1 +1,28 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+// let count = 0
+// console.log(count)
+
+// //1.create a variable, myAge and set the "value" to your age 
+// let myAge = 30
+// //2. Log thr myAge variable to the console
+// console.log(myAge)
+
+//let count = 5 + 9
+//console.log(count)
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+//1. Create two variables, myAge and humanDogRatio
+//2. Multiply the two together and store the result in myDogAge
+// 3. Log my DogAge to the console
+
+let myAge = 35
+let humanDogRatio = 7
+ 
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
