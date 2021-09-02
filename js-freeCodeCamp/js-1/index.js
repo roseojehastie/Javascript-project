a9d304597319421b5644cf6862692e13b8b35c9a
Rose Hastie <roseojehastie@gmail.com>
2021-09-02 16:59:44 +0100

    add contanate two strings in a fucntion

--- js-freeCodeCamp/js-1/index.js
+++ js-freeCodeCamp/js-1/index.js
@@ -57,3 +57,20 @@ function greetLinda(){
    console.log(greet + ", " + name + "!")
 }
 greetLinda()
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Points() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+console.log(myPoints)
