9c2825a9ff9dfab6ccdc5ee7c346589808405ed2
Rose Hastie <roseojehastie@gmail.com>
2021-09-03 23:04:22 +0100

    addloops, array and dom

--- counting-in-javascript/index.js
+++ counting-in-javascript/index.js
@@ -1,55 +1,67 @@
 
-//COUNT TO TEN!
+// //COUNT TO TEN!
 
-// WE NEED TO SPECIFY
+// // WE NEED TO SPECIFY
 
-//WHERE SHOULD WE "START" COUNTING
-//WHERE IS THE FINISH
-//WHAT IS THE STEP SIZE WE SHOULD USE?
+// //WHERE SHOULD WE "START" COUNTING
+// //WHERE IS THE FINISH
+// //WHAT IS THE STEP SIZE WE SHOULD USE?
 
-// START            FINSIH      STEP SIZE
-for ( let count =10; count <21; count += 1 ) {
-    console.log(count)
-}
-//WRITE YOUR FIRST LOOP
-// CREATE A FOR LOOP THAT COUNTS FROM 10 TO 100 IN STEPS OF 10
-//USE CONSOLE.LOG TO LOG OUT THE NUMBERS
+// // START            FINSIH      STEP SIZE
+// for ( let count =10; count <21; count += 1 ) {
+//     console.log(count)
+// }
+// //WRITE YOUR FIRST LOOP
+// // CREATE A FOR LOOP THAT COUNTS FROM 10 TO 100 IN STEPS OF 10
+// //USE CONSOLE.LOG TO LOG OUT THE NUMBERS
 
-for ( let count = 10; count <101; count += 10 ) {
-    console.log(count)
-}
-for ( i = 0; i<6; i +=1) {
-    console.log(i)
-}
-//FOR LOOP & ARRAYS
-let message = [
-    "Hey how is it going?",
-    "I am good, thank you! How about you?",
-    "All good. Been working on my portfolio lately",
-    "Same here",
-    "Great to hear.",
-    "🥰"
-]
-//DRY - DO NOT REPEAT YOURSELF E.G.
-// console.log([0])
-// console.log([1])
-// console.log([2])
-// console.log([3])
+// for ( let count = 10; count <101; count += 10 ) {
+//     console.log(count)
+// }
+// for ( i = 0; i<6; i +=1) {
+//     console.log(i)
+// }
+// //FOR LOOP & ARRAYS
+// let message = [
+//     "Hey how is it going?",
+//     "I am good, thank you! How about you?",
+//     "All good. Been working on my portfolio lately",
+//     "Same here",
+//     "Great to hear.",
+//     "🥰"
+// ]
+// //DRY - DO NOT REPEAT YOURSELF E.G.
+// // console.log([0])
+// // console.log([1])
+// // console.log([2])
+// // console.log([3])
+
+// // for ( let i = 0; i < 5; i += 1) {
+// //     console.log(message)
+// // } // not dynamic
 
 // for ( let i = 0; i < 5; i += 1) {
 //     console.log(message)
-// } // not dynamic
+// }
 
-for ( let i = 0; i < 5; i += 1) {
-    console.log(message)
-}
+// //WRITE YOUR FIRST ARRAY-BASED LOOP
+// //CREATE A FOR LOOP THAT LOGS OUT ALL THE CARDS IN THE ARRAY
+// //USE CARDS.LENGTH TO SPECIFY HOW LONG THE LOOP SHOULD RUN
 
-//WRITE YOUR FIRST ARRAY-BASED LOOP
-//CREATE A FOR LOOP THAT LOGS OUT ALL THE CARDS IN THE ARRAY
-//USE CARDS.LENGTH TO SPECIFY HOW LONG THE LOOP SHOULD RUN
+// let cards = [7, 3, 9]
 
-let cards = [7, 3, 9]
+// for (let i = 0; i <cards.length; i++) {
+//     console.log(cards)
+// }
 
-for (i = 0; i <cards.length; i++) {
-    console.log(cards)
+// //FOR LOOPS, ARRAYS AND DOM
+
+let sentence = ["Hello", "my", "name", "is", "Eke"]
+let greetingEl = document.getElementById("greeting-el")
+
+//RENDER THE SENTENCE IN THE greetingEl PARAGRAPH USING A FOR LOOP AND .textContent
+ 
+for (let i = 0; i < sentence.length; i++) {
+greetingEL.textContent += sentence[i] + " "
+
 }
