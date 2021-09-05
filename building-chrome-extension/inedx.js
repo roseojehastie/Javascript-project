e31b102e74e8771e1a4b45126f4158c0b7140a89
Rose Hastie <roseojehastie@gmail.com>
2021-09-05 01:40:09 +0100

    add box to DOM

--- building-chrome-extension/inedx.js
+++ building-chrome-extension/inedx.js
@@ -8,3 +8,13 @@ let inputBtn = document.getElementById("input-btn" )
 inputBtn.addEventListener("click", function () {
     console.log("Button clicked from addEventListener")
 })
+//Grab the box from DOM store it in the variable
+// Add a click event listener to the box
+// Log out "I want to open the box" when it is clicked
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function() {
+    console.log("I want to open the box")
+})
+
